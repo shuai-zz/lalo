297244a26b781564d6225a3e6076a6f71a14e001
shuai <s74933347@gmail.com>
2026-08-22 13:16:52 +0800

    feat: export meshes as binary STL (#6)

--- src/lalo/__init__.py
+++ src/lalo/__init__.py
@@ -1,5 +1,6 @@
 """Lalo geometry tools."""
 
 from lalo.meshing import Mesh, mesh_occupancy
+from lalo.stl import binary_stl_bytes, write_binary_stl
 
-__all__ = ["Mesh", "mesh_occupancy"]
+__all__ = ["Mesh", "binary_stl_bytes", "mesh_occupancy", "write_binary_stl"]
