ce2ecf9e3fc68a5804fa12d280074cf0e17d0286
shuai <s74933347@gmail.com>
2026-08-22 13:19:27 +0800

    feat: generate solid cuboid occupancy grids (#10)

--- src/lalo/__init__.py
+++ src/lalo/__init__.py
@@ -2,5 +2,13 @@
 
 from lalo.meshing import Mesh, mesh_occupancy
 from lalo.stl import binary_stl_bytes, write_binary_stl
+from lalo.voxel import OccupancyGrid, solid_cuboid
 
-__all__ = ["Mesh", "binary_stl_bytes", "mesh_occupancy", "write_binary_stl"]
+__all__ = [
+    "Mesh",
+    "OccupancyGrid",
+    "binary_stl_bytes",
+    "mesh_occupancy",
+    "solid_cuboid",
+    "write_binary_stl",
+]
