1c92cbf0695f1afd4a235d94d99e2e463f96c29d
shuai <s74933347@gmail.com>
2026-08-22 13:22:30 +0800

    feat: define canonical humanoid layout (#12)

--- src/lalo/__init__.py
+++ src/lalo/__init__.py
@@ -1,12 +1,15 @@
 """Lalo geometry tools."""
 
+from lalo.body import CANONICAL_PARTS, PartSpec
 from lalo.meshing import Mesh, mesh_occupancy
 from lalo.stl import binary_stl_bytes, write_binary_stl
 from lalo.voxel import OccupancyGrid, solid_cuboid
 
 __all__ = [
+    "CANONICAL_PARTS",
     "Mesh",
     "OccupancyGrid",
+    "PartSpec",
     "binary_stl_bytes",
     "mesh_occupancy",
     "solid_cuboid",
